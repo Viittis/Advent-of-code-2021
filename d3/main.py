ed470258a6802918d2a4fef5e320e533659c6fb1
Marko V <viittis@gmail.com>
2021-12-03 15:03:54 +0200

    part2 scheming

--- d3/main.py
+++ d3/main.py
@@ -1,7 +1,7 @@
 # https://adventofcode.com/2021/day/3
 from collections import Counter
 
-data_in = 'data.txt'  # External data file
+data_in = 'data2.txt'  # External data file
 
 
 def load_data(file_in):
@@ -28,10 +28,41 @@ def part_one(data):
     return int(gamma_rate, 2), int(epsilon_rate, 2)
 
 
+def exhaustive_loop(data, val):
+    result = ''
+
+    if val == 'ogr':
+        idx = 0
+    else:
+        idx = -1
+
+    for index in range(len(data[0])):
+        counts = Counter([x[index] for x in data])
+        print(list(counts.values()))
+
+        if all(x == list(counts.values())[0] for x in list(counts.values())):
+            result += str(idx+1)
+        else:
+            result += str(counts.most_common()[idx][0])
+
+    print(result)
+    return int(result, 2)
+
+
 def part_two(data):
     """Solution for the 2nd task."""
+    first_bits = Counter([x[0] for x in data])
 
-    print('')
+    ogr_val = first_bits.most_common()[0][0]
+    csr_val = first_bits.most_common()[-1][0]
+
+    ogr_data = [x for x in data if x[0] == ogr_val]
+    csr_data = [x for x in data if x[0] == csr_val]
+
+    ogr_value = exhaustive_loop(ogr_data, 'ogr')
+    csr_value = exhaustive_loop(csr_data, 'csr')
+
+    print(ogr_value, csr_value)
 
 
 def main():
@@ -39,7 +70,7 @@ def main():
     data = load_data(data_in)
 
     gamma, epsilon = part_one(data)
-    print(f'Answer is {gamma * epsilon}')  # 3813416
+    #print(f'Answer is {gamma * epsilon}')  # 3813416
 
     part_two(data)
 
